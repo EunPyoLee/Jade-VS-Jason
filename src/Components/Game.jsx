8a705bc4aa950ab87d0121e40d51fab625913c24
eunpyo <eunpyo@umich.edu>
2020-05-05 22:03:25 -0400

    Turn-based system deployment, size problem fixed, used replay attack preventing timestamp

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import Matter from 'matter-js';
-// import './App.css';
 
 
-function percentX(percent) {
-  return Math.round(percent / 100 * window.innerWidth);
+function percentXtoRender(percent, renderWidth) {
+  return (percent / 100 * renderWidth);
 }
-function percentXtoRender(percent, renderWidth){
-  return Math.round(percent / 100 * renderWidth);
-}
-function percentY(percent) {
-  return Math.round(percent / 100 * window.innerHeight);
-}
-function percentYtoRender(percent, renderHeight){
-  return Math.round(percent / 100 * renderHeight);
+
+function percentYtoRender(percent, renderHeight) {
+  return (percent / 100 * renderHeight);
 }
 
 class Game extends React.Component {
@@ -26,7 +20,6 @@ class Game extends React.Component {
     var Engine = Matter.Engine,
       Render = Matter.Render,
       Runner = Matter.Runner,
-      Composites = Matter.Composites,
       Events = Matter.Events,
       Constraint = Matter.Constraint,
       MouseConstraint = Matter.MouseConstraint,
@@ -39,8 +32,8 @@ class Game extends React.Component {
 
 
     // create renderer
-    let renderWidth = percentX(100);
-    let renderHeight = percentY(100);
+    let renderWidth = 900;
+    let renderHeight = 600;
     var render = Render.create({
       canvas: document.querySelector('#gameCanvas'),
       engine: engine,
@@ -51,40 +44,91 @@ class Game extends React.Component {
       }
     });
 
-    
+
 
     Render.run(render);
     var runner = Runner.create();
     Runner.run(runner, engine);
 
-
-    var ground = Bodies.rectangle(150, 600, percentXtoRender(100, renderWidth), 50, { isStatic: true }),
-      rockOptions = { density: 0.004, restitution: 0.8 },
-      rock1 = Bodies.circle(percentXtoRender(0, renderWidth), percentYtoRender(50, renderHeight), 10, rockOptions),
-      anchor1 = { x: percentXtoRender(0, renderWidth), y:percentYtoRender(50, renderHeight)},
-      elastic1 = Constraint.create({
+    const ground = Bodies.rectangle(renderWidth / 2.0, renderHeight + 110, renderWidth * 50, 200, { isStatic: true });
+    ground.label = "ground";
+    let rockOptions = { density: 0.004, restitution: 0.4 };
+    let rock1 = Bodies.circle(percentXtoRender(10, renderWidth), percentYtoRender(80, renderHeight), 8, rockOptions);
+    rock1.label = "userRock";
+    let anchor1 = { x: percentXtoRender(10, renderWidth), y: percentYtoRender(80, renderHeight) };
+    let elastic1 = Constraint.create({
         pointA: anchor1,
         bodyB: rock1,
-        stiffness: 0.05
-      }),
-      rock2 = Bodies.circle(percentXtoRender(51, renderWidth), percentYtoRender(50, renderHeight), 10, rockOptions),
-      anchor2 = { x: percentXtoRender(51, renderWidth), y:percentYtoRender(50, renderHeight)},
-      elastic2 = Constraint.create({
+        stiffness: 0.06
+      });
+    let rock2 = Bodies.circle(percentXtoRender(100, renderWidth), percentYtoRender(80, renderHeight), 8, rockOptions);
+    rock2.label = "compRock";
+    let anchor2 = { x: percentXtoRender(100, renderWidth), y: percentYtoRender(80, renderHeight) };
+    let elastic2 = Constraint.create({
         pointA: anchor2,
         bodyB: rock2,
-        stiffness: 0.05
+        stiffness: 0.06
       });
+    Matter.Body.setStatic(rock2, true);
 
-    var ground2 = Bodies.rectangle(percentXtoRender(25, renderWidth), percentYtoRender(40, renderHeight), 25, 400, { isStatic: true });
+    let isUserTurn = true;
+    const ground2 = Bodies.rectangle(percentXtoRender(55, renderWidth), percentYtoRender(80, renderHeight), 15, 400, { isStatic: true });
     World.add(engine.world, [ground, ground2, rock1, rock2, elastic1, elastic2]);
+    var collision = Matter.SAT.collides(rock1, ground);
+    if(collision.collided){
+        console.log("Hi");
+    }
+
 
     Events.on(engine, 'afterUpdate', function () {
-      if (mouseConstraint.mouse.button === -1 && (Math.abs(rock1.position.x - 10) > 20 || Math.abs(rock2.position.y - 480) > 10)) {
-        rock1 = Bodies.circle(percentXtoRender(0, renderWidth), percentYtoRender(50, renderHeight), 10, rockOptions);
+      if (mouseConstraint.mouse.button === -1 && (Math.abs(rock1.position.x - percentXtoRender(10, renderWidth)) > 5 || Math.abs(rock1.position.y - percentYtoRender(80, renderHeight)) > 5)) {
+        rock1 = Bodies.circle(percentXtoRender(10, renderWidth), percentYtoRender(80, renderHeight), 8, rockOptions);
         // World.add(engine.world, rock);
         elastic1.bodyB = rock1;
       }
+      if (mouseConstraint.mouse.button === -1 && (Math.abs(rock2.position.x - percentXtoRender(100, renderWidth)) > 5 || Math.abs(rock2.position.y - percentYtoRender(80, renderHeight)) > 5)) {
+        rock2 = Bodies.circle(percentXtoRender(100, renderWidth), percentYtoRender(80, renderHeight), 8, rockOptions);
+        // World.add(engine.world, rock);
+        elastic2.bodyB = rock2;
+      }
     });
+    let lastUserTurnTime = Date.now();
+    let lastCompTurnTime = Date.now();
+
+    Events.on(engine, 'collisionStart', function (event) {
+        let pairs = event.pairs;
+        console.log(pairs[0]);
+        if(pairs[0].bodyA.label === "ground" || pairs[0].bodyB.label === "ground"){
+            let thrownRock = pairs[0].bodyA.label === "ground" ? pairs[0].bodyB : pairs[0].bodyA;
+            let ballLabel = thrownRock.label;
+            setTimeout(function(){
+                World.remove(engine.world, thrownRock);
+                if(isUserTurn && ballLabel === "userRock" && Math.abs(lastUserTurnTime - Date.now()) > 3000){
+                    isUserTurn = false;
+                    lastUserTurnTime = Date.now();
+                    rock1 =  Bodies.circle(percentXtoRender(10, renderWidth), percentYtoRender(80, renderHeight), 8, rockOptions);
+                    Matter.Body.setStatic(rock1, true);
+                    Matter.Body.setStatic(rock2, false);
+                    rock1.label = "userRock";
+                    World.add(engine.world, rock1);
+                    elastic1.bodyB = rock1;
+
+                }else if (!isUserTurn && ballLabel === "compRock" && Math.abs(lastCompTurnTime - Date.now()) > 3000){
+                    isUserTurn = true;
+                    lastCompTurnTime = Date.now();
+                    rock2 = Bodies.circle(percentXtoRender(100, renderWidth), percentYtoRender(80, renderHeight), 8, rockOptions);
+                    Matter.Body.setStatic(rock2, true);
+                    Matter.Body.setStatic(rock1, false);
+                    rock2.label = "compRock";
+                    World.add(engine.world, rock2);
+                    elastic2.bodyB = rock2;
+                }
+                console.log("remove printout\n");
+            }, 3000);
+        }
+      });
+  
+      
 
     // add mouse control
     var mouse = Mouse.create(render.canvas),
@@ -99,6 +143,9 @@ class Game extends React.Component {
       }
       );
 
+    mouse.element.removeEventListener("mousewheel", mouse.mousewheel);
+    mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel);
+
     World.add(world, mouseConstraint);
 
     // keep the mouse in sync with rendering
@@ -106,7 +153,7 @@ class Game extends React.Component {
 
     // fit the render viewport to the scene
     Render.lookAt(render, {
-      position: {x : 500, y: 500},
+      position: { x: 500, y: 500 },
       min: { x: 0, y: 0 },
       max: { x: 1000, y: 600 }
     }
